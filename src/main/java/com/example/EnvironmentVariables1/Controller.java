29f62d443ae3fec85fc8d4319699c405f29f3629
TTranchese <tommaso.tranchese.96@gmail.com>
2023-05-19 03:07:25 +0200

    initial commit

---
+++ src/main/java/com/example/EnvironmentVariables1/Controller.java
@@ -0,0 +1,18 @@
+package com.example.EnvironmentVariables1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+	@Autowired
+	public Environment environment;
+	@GetMapping
+	public String getController(){
+		return "Hi! authCode:"+environment.getProperty("myVar.authCode")+" dev:"+environment.getProperty("myVar.devName");
+	}
+}
